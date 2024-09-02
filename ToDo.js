21da66e17247a7f63de355115ce591ffc646cd76
Priyanshu Raturi <130962792+Priyanshur36@users.noreply.github.com>
2024-09-02 23:16:46 +0530

    Delete ToDo.js

--- ToDo.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const TodoSchema = new mongoose.Schema({
-    title: { type: string,required: true},
-    completed:{type:Boolean,default:false},
-    });
-const Todo= mongose.model('Todo',TodoSchema);
-export default Todo;
